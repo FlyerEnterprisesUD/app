15a3ecf83be1944c98c58f4fddd357546cff704d
Ryan Farrar <farrarr1@udayton.edu>
2017-02-23 16:16:57 -0500

    Stus Changes

--- src/screens/StusLanding.js
+++ src/screens/StusLanding.js
@@ -1,47 +1,7 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet, Navigator } from 'react-native';
+import { StyleSheet, Text, View, TextInput, AsyncStorage, TouchableOpacity, Dimensions, Navigator } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
-let menu = {
-    'products': [
-      {
-        'name': 'Sunny & 55',
-        'group': 'Berry Blast',
-        'ingredients': [
-          'Mango',
-          'Strawberry',
-          'Orange Juice',
-          'Greek Yogurt'
-        ]
-      },
-      {
-        'name': 'Cruising',
-        'group': 'Berry Blast',
-        'ingredients': [
-          'Pineapple',
-          'Raspberry',
-          'Apple Juice'
-        ]
-      },
-      {
-        'name': 'MVP',
-        'group': 'Berry Blast',
-        'ingredients': [
-          'Mango',
-          'Pineapple',
-          'Almond Milk'
-        ]
-      }
-    ]
-};
-
-let about = {
-  'location': 'Recplex',
-  'hours': [
-    'Sunday - Thursday 11am - 11pm',
-    'Friday - Saturday 11am - 9pm'
-  ]
-};
 
 class StusLanding extends Component {
   navigateToAbout() {
