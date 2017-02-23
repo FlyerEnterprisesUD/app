88af44ec263e5c0e86eb28b92ed39af8c6a99172
Ryan Farrar <farrarr1@udayton.edu>
2017-02-22 20:42:57 -0500

    New Logo
    New Logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -7,27 +7,29 @@ const NavigationBar = (toggleSideMenu) => {
   const LeftButton = () => {
     return (
       <TouchableHighlight
-        style={{marginTop: 7, marginLeft: 9}}
+        style={{marginTop: 12, marginLeft: 9}}
+        //iPhone should have topMargin of 7
         onPress={toggleSideMenu}
         underlayColor='transparent'>
         <Icon
           color='white'
           name='menu'
-          size={28}
+          size={38}
+          //iPhone should be size 28
         />
       </TouchableHighlight>
     );
   };
 
   const RightButton = () => {
-    const fe = require('../images/fe.png');
+    const fe = require('../images/wfe2.png');
     return (
       <Image
         source={fe}
         style={styles.logo}/>
     );
   };
-//hello
+
   const Title = (route) => {
     return (
       <NavBarTitle style={styles.title} title={route.id} {...route.passProps} />
@@ -52,13 +54,33 @@ let styles = StyleSheet.create({
     backgroundColor: '#CC0F40',
     ...Platform.select({
       android: {
-        height: 55
+        height: 65
       }
     })
   },
+<<<<<<< HEAD
+  backIcon: {
+    marginLeft: 10,
+    marginTop: 10,
+    ...Platform.select({
+      android: {
+        marginTop: 15
+        //orginially 15
+      }
+    })
+  },
+=======
+>>>>>>> origin/master
   logo: {
     width: 45,
-    height: 45
+    height: 45,
+    ...Platform.select({
+      android: {
+        width: 48,
+        height: 48,
+        marginRight: 10,
+        marginTop: 8
+      } })
   }
 });
 
